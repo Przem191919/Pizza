73f66838b0178d235634b9c073a485cb5fb253bd
przem <przem.olejnik@gmail.com>
2023-06-19 17:27:18 +0200

    finishNav startHome.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,37 @@
-import React from 'react';
-import Logo from '../assets/logo.png'
+import React, { useState } from 'react';
+import Logo from '../assets/logo.png';
 import { Link } from 'react-router-dom';
+import { FaBars, FaTimes} from 'react-icons/fa';
 import "../styles/Navbar.css";
 
 
+
 function Navbar() {
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
+
   return (
     <div className='navbar'>
-      <div className='leftNav'>
+      <div className='leftNav' id={openLinks ? "open" : "close"}>
         <img src={Logo} />
+        <div className='hiddenLinks'>
+        <Link to="/">Home</Link>
+        <Link to="/menu">Menu</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        </div>
       </div>
       <div className='rightNav'>
         <Link to="/">Home</Link>
         <Link to="/menu">Menu</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
+        <button onClick={toggleNavbar}>
+          <FaBars/>
+        </button>
       </div>
     </div>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Home() {
   return (
-    <div>
-      hhhhhdfdgfgfgssfsdsds
+    <div className='home'>
+      <div className='headerContainer' style={{ backgroundImage: }}>
+        <h1>Leonardo's Pizza</h1>
+        <p>PIZZA SUPREME</p>
+        <Link to="/menu">
+          <button>ORDER NOW</button>
+        </Link>
+      </div>
       
     </div>
   )
