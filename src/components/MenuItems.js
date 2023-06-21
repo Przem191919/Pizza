4987165602ca01e3b2478c832c84f20647e6714f
przem <przem.olejnik@gmail.com>
2023-06-21 16:10:22 +0200

    MenuItems

---
+++ src/components/MenuItems.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function MenuItems(image, name, price) {
+  return (
+    <div className='menuItem'>
+      <div></div>
+      <h1></h1>
+      <p></p>
+    </div>
+  )
+}
+
+export default MenuItems
---
+++ src/helpers/MenuList.js
@@ -0,0 +1,40 @@
+import Pepperoni from "../assets/pepperoni.jpg";
+import Margherita from "../assets/margerita.jpg";
+import LeonardoSpezial from "../assets/leonardotechSpezial.jpg"
+import Vegan from "../assets/vegan.jpg";
+import Pieneaple from "../assets/pineaple.jpg";
+import Expensive from "../assets/expensive.jpg";
+
+
+export const MenuList = [
+    {
+        name: "Pepperoni Pizza",
+        image: Pepperoni,
+        price: 15.99,
+    },
+    {
+        name: "Margherita Pizza",
+        image: Margherita,
+        price: 11.99,
+    },
+    {
+        name: "LeonardoSpecial Pizza",
+        image: LeonardoSpezial,
+        price: 25.99,
+    },
+    {
+        name: "Vegan Pizza",
+        image: Vegan,
+        price: 17.99,
+    },
+    {
+        name: "Pineaple Pizza",
+        image: Pieneaple,
+        price: 4.99,
+    },
+    {
+        name: "Expensive Pizza",
+        image: Expensive,
+        price: 45.99,
+    },
+];
